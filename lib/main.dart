e2219088cb651af8b20403f228d6c840349572d8
moha_kh_23 <khrijimohamedahmed@gmail.com>
2024-04-24 21:25:45 +0200

    commit 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'models/task_data.dart';
+import 'screens/tasks_screen.dart';
 
-import 'TaskScreen.dart';
-
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: TaskScreen(),
+    return ChangeNotifierProvider<TaskData>.value(
+      value: TaskData(),
+      child: MaterialApp(
+        home: TasksScreen(),
+      ),
     );
   }
 }
